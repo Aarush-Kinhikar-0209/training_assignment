39347cd2ef88e2998316117f8b032511b373e829
Aarush-Kinhikar-0209 <127879814+Aarush-Kinhikar-0209@users.noreply.github.com>
2025-01-23 15:02:22 +0530

    Delete src/main/java/com/assignment/service/PremiumUserService.java

--- src/main/java/com/assignment/service/PremiumUserService.java
+++
@@ -1,8 +0,0 @@
-package com.assignment.service;
-
-import java.util.Optional;
-import com.assignment.entity.User;
-
-public interface PremiumUserService {
-    Optional<User> register(String name, String email, String password);
-}
