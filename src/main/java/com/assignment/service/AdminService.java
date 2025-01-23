f3c6a6802b76de633e653ceeafc295e04d92f4ff
Aarush-Kinhikar-0209 <127879814+Aarush-Kinhikar-0209@users.noreply.github.com>
2025-01-23 15:02:10 +0530

    Delete src/main/java/com/assignment/service/AdminService.java

--- src/main/java/com/assignment/service/AdminService.java
+++
@@ -1,8 +0,0 @@
-package com.assignment.service;
-
-import java.util.Optional;
-import com.assignment.entity.User;
-
-public interface AdminService {
-    Optional<User> register(String name, String email, String password);
-}
