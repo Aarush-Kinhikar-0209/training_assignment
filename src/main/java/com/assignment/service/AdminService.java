9a5de2a7e78ff392924038fabf735203dae12167
Aarush-Kinhikar-0209 <127879814+Aarush-Kinhikar-0209@users.noreply.github.com>
2025-01-23 15:04:54 +0530

    Update AdminService.java

--- src/main/java/com/assignment/service/AdminService.java
+++ src/main/java/com/assignment/service/AdminService.java
@@ -1,9 +1,5 @@
 package com.assignment.service;
 
-// import java.util.Optional;
-
-// import com.assignment.entity.User;
-
 public interface AdminService extends UserService{
-    // Optional<User> register(String name, String email, String password);
+    // methods only for admin user
 }
