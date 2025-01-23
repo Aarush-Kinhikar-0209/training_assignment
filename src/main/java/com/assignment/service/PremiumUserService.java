1b1e8cebe997d2a48abf7743989aaadc3be662a8
Aarush-Kinhikar-0209 <127879814+Aarush-Kinhikar-0209@users.noreply.github.com>
2025-01-23 15:04:24 +0530

    Update PremiumUserService.java

--- src/main/java/com/assignment/service/PremiumUserService.java
+++ src/main/java/com/assignment/service/PremiumUserService.java
@@ -1,9 +1,5 @@
 package com.assignment.service;
 
-// import java.util.Optional;
-
-// import com.assignment.entity.User;
-
 public interface PremiumUserService extends UserService {
-     // Optional<User> register(String name, String email, String password);
+     // methods for premium users only
 }
