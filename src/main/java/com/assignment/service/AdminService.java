4adef82419356927ea65b87b60249ce60ab63895
Aarush-Kinhikar-0209 <127879814+Aarush-Kinhikar-0209@users.noreply.github.com>
2025-01-23 15:03:10 +0530

    Add files via upload

---
+++ src/main/java/com/assignment/service/AdminService.java
@@ -0,0 +1,9 @@
+package com.assignment.service;
+
+// import java.util.Optional;
+
+// import com.assignment.entity.User;
+
+public interface AdminService extends UserService{
+    // Optional<User> register(String name, String email, String password);
+}
